15750b87ea1dc5f0cdfd3a4511006ac421ca1770
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-08-22 17:34:01 +0700

    updating json

--- schools_scraper.py
+++ schools_scraper.py
@@ -1,17 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import json
 
 
-url = 'http://www.ecolia.ma/ecole-maternelle-du-groupe-scolaire-de-la-residence-3-674'
+url = 'http://www.ecolia.ma/ecole-etablissement-el-bilia-931'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 
 def contact_scraper(selector):
-    contact = infos.find(class_=selector)
+    contact = bloc_info.find(class_=selector)
     contact_list = []
-    contact_list.append(contact.get_text().encode("utf-8"))
+    contact_list.append(contact.get_text())
 
     # other contact infos
     if contact.find_parent().find("ul", {"class": "dropdown"}):
@@ -19,7 +19,7 @@ def contact_scraper(selector):
         for item in contact.find_parent().select("ul", {"class": "dropdown"}):
             for other_nums in item.select("li"):
                 if other_nums.get_text() != ' ':
-                    contact_list.append(other_nums.get_text().encode("utf-8"))
+                    contact_list.append(other_nums.get_text())
 
     return contact_list
 
@@ -31,48 +31,54 @@ def get_link(a):
         return ''
 
 
-with open('schools.csv', 'w') as csv_file:
-    # get the name
-    name = soup.find(class_="denomination").get_text().encode("utf-8")
+info = {}
 
-    # Levels
-    levels = ''
-    all_levels = soup.select(".entite-existe")
-    for label in all_levels:
-        levels = "{}, {}".format(levels, label.find(
-            "span").get_text()).encode("utf-8")
+# get the name
+info['name'] = soup.find(class_="denomination").get_text()
 
-    # bloc with the main information
-    infos = soup.find(class_="ecole-bloc-infos").contents[3]
 
-    # private or public institution
-    cost = soup.find(
-        id="type-etablissement").contents[3].get_text().encode("utf-8")
+# Levels
+levels = ''
+all_levels = soup.select(".entite-existe")
+for label in all_levels:
+    if levels != '':
+        info['levels'] = "{}, {}".format(levels, label.find("span").get_text())
+    else:
+        info['levels'] = label.find("span").get_text()
 
-    # the address
-    address = infos.find("div", {"title": "adresse"}).find(
-        class_="adresse").get_text().encode("utf-8")
 
-    # phone numbers (list)
-    phone_numbers = contact_scraper("telephone")
+# bloc with the main information
+bloc_info = soup.find(class_="ecole-bloc-infos").contents[3]
 
-    # emails (list)
-    emails = contact_scraper("email")
+# private or public institution
+info['type'] = soup.find(
+    id="type-etablissement").contents[3].get_text()
 
-    # facebook
-    facebook_link = infos.find(
-        'span', {"class": "fa-facebook"}).find_next_sibling().contents[0]
+# the address
+info['address'] = bloc_info.find("div", {"title": "adresse"}).find(
+    class_="adresse").get_text()
 
-    facebook = get_link(facebook_link).encode("utf-8")
+# phone numbers (list)
+info['phone numbers'] = contact_scraper("telephone")
 
-    # website
-    website_link = infos.find('span', {"class": "site-web"}).find("a")
-    website = get_link(website_link).encode("utf-8")
+# emails (list)
+info['emails'] = contact_scraper("email")
 
-    # csv
-    csv_writer = csv.writer(csv_file)
-    headers = ["name", "address", "type", "cost",
-               "phone numbers", "emails", "facebook", "website"]
-    csv_writer.writerow(headers)
-    csv_writer.writerow([name, address, cost, levels,
-                         phone_numbers, emails, phone_numbers, facebook, website])
+# facebook
+facebook_link = bloc_info.find(
+    'span', {"class": "fa-facebook"}).find_next_sibling().contents[0]
+info['facebook'] = get_link(facebook_link)
+
+# website
+website_link = bloc_info.find('span', {"class": "site-web"}).find("a")
+info['website'] = get_link(website_link)
+
+
+# saving to json
+with open('schools.json', encoding="utf-8") as f:
+    data = json.load(f)
+    data['institutes'].append(info)
+    print(data)
+
+with open('schools.json', 'w', encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
