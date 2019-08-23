46d1c3b8a282dcc5a584f32cbc704c785a37b239
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-08-23 21:03:16 +0700

    scraping all the pages

--- schools_scraper.py
+++ schools_scraper.py
@@ -3,15 +3,14 @@ from bs4 import BeautifulSoup
 import json
 
 
-url = 'http://www.ecolia.ma/ecole-etablissement-el-bilia-931'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
-
 def contact_scraper(selector):
     contact = bloc_info.find(class_=selector)
     contact_list = []
-    contact_list.append(contact.get_text())
+
+    if contact:
+        contact_list.append(contact.get_text())
+    else:
+        return ''
 
     # other contact infos
     if contact.find_parent().find("ul", {"class": "dropdown"}):
@@ -31,54 +30,96 @@ def get_link(a):
         return ''
 
 
-info = {}
+def scraping():
+    info = {}
 
-# get the name
-info['name'] = soup.find(class_="denomination").get_text()
+    name = soup.find(class_="denomination").get_text()
+
+    city = soup.find(class_="fa-map-marker").find_next_sibling().text
+    city = city[0:10]
+
+    if city == "Casablanca":
+
+        # get the name
+        info['name'] = name
+        info['city'] = city
+
+        # get the logo
+        logo = soup.find('img', {"class": "img-container"})["src"]
+        logo = "http://www.ecolia.ma{}".format(logo[2:])
+
+        if logo != 'http://www.ecolia.ma/annuaire/logo/logo-v2.png':
+            info['logo'] = logo
+        else:
+            info['logo'] = ''
+
+        # Levels
+        levels = ''
+        all_levels = soup.select(".entite-existe")
+        for label in all_levels:
+            if levels != '':
+                info['levels'] = "{}, {}".format(
+                    levels, label.find("span").get_text())
+            else:
+                info['levels'] = label.find("span").get_text()
+
+        # private or public institution
+        info['type'] = soup.find(
+            id="type-etablissement").contents[3].get_text()
+
+        # the address
+        info['address'] = bloc_info.find("div", {"title": "adresse"}).find(
+            class_="adresse").get_text()
+
+        # phone numbers (list)
+        info['phone numbers'] = contact_scraper("telephone")
+
+        # emails (list)
+        info['emails'] = contact_scraper("email")
+
+        # facebook
+        try:
+            facebook_link = bloc_info.find(
+                'span', {"class": "fa-facebook"}).find_next_sibling().contents[0]
+            info['facebook'] = get_link(facebook_link)
+        except AttributeError:
+            facebook_link = ''
+
+        # website
+        try:
+            website_link = bloc_info.find(
+                'span', {"class": "site-web"}).find("a")
+            info['website'] = get_link(website_link)
+        except AttributeError:
+            website_link = ''
+
+    # saving to json
+        with open('schools.json',  encoding="utf-8") as f:
+            data = json.load(f)
+            data['institutes'].append(info)
+
+        with open('schools.json', 'w', encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-# Levels
-levels = ''
-all_levels = soup.select(".entite-existe")
-for label in all_levels:
-    if levels != '':
-        info['levels'] = "{}, {}".format(levels, label.find("span").get_text())
-    else:
-        info['levels'] = label.find("span").get_text()
+def get_urls(page):
+    response = requests.get(page)
+    bsoup = BeautifulSoup(response.text, "html.parser")
+
+    tags = bsoup.find(id="list-ecole-content").select("h3")
+    urls = []
+    for h3 in tags:
+        urls.append("http://www.ecolia.ma/{}".format(h3.contents[0]['href']))
+
+    return urls
 
 
-# bloc with the main information
-bloc_info = soup.find(class_="ecole-bloc-infos").contents[3]
+for i in range(2, 50):
+    urls = get_urls("http://www.ecolia.ma/ecoles-page-{}".format(i))
 
-# private or public institution
-info['type'] = soup.find(
-    id="type-etablissement").contents[3].get_text()
+    for url in urls:
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        bloc_info = soup.find(class_="ecole-bloc-infos").contents[3]
 
-# the address
-info['address'] = bloc_info.find("div", {"title": "adresse"}).find(
-    class_="adresse").get_text()
-
-# phone numbers (list)
-info['phone numbers'] = contact_scraper("telephone")
-
-# emails (list)
-info['emails'] = contact_scraper("email")
-
-# facebook
-facebook_link = bloc_info.find(
-    'span', {"class": "fa-facebook"}).find_next_sibling().contents[0]
-info['facebook'] = get_link(facebook_link)
-
-# website
-website_link = bloc_info.find('span', {"class": "site-web"}).find("a")
-info['website'] = get_link(website_link)
-
-
-# saving to json
-with open('schools.json', encoding="utf-8") as f:
-    data = json.load(f)
-    data['institutes'].append(info)
-    print(data)
-
-with open('schools.json', 'w', encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+        scraping()
